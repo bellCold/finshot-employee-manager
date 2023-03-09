778688e59a1c084c0c7218a44d84f75dfebff26a
whdcks420 <whdcks420@gmail.com>
2023-03-10 00:36:59 +0900

    comma constant 수정

--- src/main/java/com/example/finshot/global/constants/FinShotConstant.java
+++ src/main/java/com/example/finshot/global/constants/FinShotConstant.java
@@ -2,6 +2,6 @@ package com.example.finshot.global.constants;
 
 public class FinShotConstant {
     public static final String LF = "\n";
-    public static final String COMMA = ", ";
+    public static final String COMMA = ",";
     public static final String CSV_HEADER = "직원 번호, 직급, 이름, 전화번호, 이메일";
 }
