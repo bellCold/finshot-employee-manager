31f8652b70bf6e6687ce9903286bab170d979de5
whdcks420 <whdcks420@gmail.com>
2023-03-09 15:27:38 +0900

    querydsl 제거

--- src/main/java/com/example/finshot/domain/Employee/EmployeeRepository.java
+++ src/main/java/com/example/finshot/domain/Employee/EmployeeRepository.java
@@ -4,7 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface EmployeeRepository extends JpaRepository<Employee, Long>, EmployeeRepositoryCustom {
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
     List<Employee> findAllByOrderByName();
 
     List<Employee> findByNameContaining(String searchWord);
